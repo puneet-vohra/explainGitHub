714818b8505fca6d48da12368b80d24e637f4209
admin <admin@admin.local>
2014-12-19 07:46:39 +0700

    Initial Commit

---
+++ explainGitHub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  explainGitHub
+//
+//  Created by admin on 12/19/2557 BE.
+//  Copyright (c) 2557 MUIDS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
